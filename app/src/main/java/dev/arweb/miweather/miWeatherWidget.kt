3749ca86dec7787615d4bd75f917d749dbb545fb
ARWeb <contact@arweb.dev>
2022-11-18 19:38:07 +0100

    restructure

--- app/src/main/java/dev/arweb/miweather/miWeatherWidget.kt
+++ app/src/main/java/dev/arweb/miweather/miWeatherWidget.kt
@@ -8,11 +8,21 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
+import java.time.LocalDateTime
 
 /**
  * Implementation of App Widget functionality.
  */
 class MiWeatherWidget : AppWidgetProvider() {
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -22,6 +32,42 @@ class MiWeatherWidget : AppWidgetProvider() {
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
+    }
+
+    // update all widgets
+    private fun updateWidgets(context: Context) {
+        val manager = AppWidgetManager.getInstance(context)
+        val ids = manager.getAppWidgetIds(ComponentName(context, javaClass))
+        // update each widget
+        ids.forEach { id -> updateAppWidget(context, manager, id) }
+    }
+
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        Log.d("MIWEATHER", "updating app widget")
+
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.mi_weather_widget)
+        //views.setTextViewText(R.id.textViewTime, currentTime)
+
+        // launch pending intent to increase value stored in shared prefs
+        views.setOnClickPendingIntent(R.id.refreshButton, pendingIntent(context, "refresh"))
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    private fun pendingIntent(context: Context?, action: String): PendingIntent? {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+
+        // return pending intent
+        return PendingIntent.getBroadcast(
+            context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+        )
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -34,66 +80,20 @@ class MiWeatherWidget : AppWidgetProvider() {
             // refresh weather data
 
             val views = RemoteViews(context.packageName, R.layout.mi_weather_widget)
-            //clock = views.
+            views.setInt(
+                R.id.refreshButton, "setImageDrawable",
+                R.drawable.ic_launcher_foreground
+            )
+            views.setInt(R.id.refreshButton, "setImageAlpha", 50)
 
-            //Log.d("MIWEATHER", "Time: "+clock.getText())
+            val cTime = LocalDateTime.now()
+            Log.d("MIWEATHER", "Time: $cTime")
 
             // update widgets
             updateWidgets(context)
         }
     }
 
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-
-    // update all widgets
-    private fun updateWidgets(context: Context) {
-        val manager = AppWidgetManager.getInstance(context)
-        val ids = manager.getAppWidgetIds(ComponentName(context, javaClass))
-        // update each widget
-        ids.forEach { id -> updateAppWidget(context, manager, id) }
-    }
-
-    private fun pendingIntent(
-        context: Context?,
-        action: String
-    ): PendingIntent? {
-        val intent = Intent(context, javaClass)
-        intent.action = action
-
-        // return pending intent
-        return PendingIntent.getBroadcast(
-            context, 0, intent,  PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
-        )
-    }
-
-    private fun updateAppWidget(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
-    ) {
-        Log.d("MIWEATHER", "updating app widget")
-
-        // get widget text from shared prefs
-        //val prefs = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-        //val widgetText = prefs.getString("widgetText", "0")
-
-        // Construct the RemoteViews object
-        val views = RemoteViews(context.packageName, R.layout.mi_weather_widget)
-        //views.setTextViewText(R.id.textViewTime, currentTime)
-
-        // launch pending intent to increase value stored in shared prefs
-        views.setOnClickPendingIntent(R.id.refreshButton, pendingIntent(context, "refresh"))
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
 }
 
 
