ef55a5574c82d9fb98721577f94f5104a433bff7
ARWeb <contact@arweb.dev>
2022-11-21 18:17:30 +0100

    api call working

--- app/src/main/java/dev/arweb/miweather/miWeatherWidget.kt
+++ app/src/main/java/dev/arweb/miweather/miWeatherWidget.kt
@@ -8,6 +8,10 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
+import android.widget.Toast
+import com.beust.klaxon.Klaxon
+import okhttp3.*
+import java.io.IOException
 import java.time.LocalDateTime
 
 /**
@@ -15,8 +19,11 @@ import java.time.LocalDateTime
  */
 class MiWeatherWidget : AppWidgetProvider() {
 
+    private val httpClient = OkHttpClient()
+
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        Toast.makeText(context, "miWeather created", Toast.LENGTH_LONG).show()
     }
 
     override fun onDisabled(context: Context) {
@@ -48,6 +55,7 @@ class MiWeatherWidget : AppWidgetProvider() {
         appWidgetId: Int
     ) {
         Log.d("MIWEATHER", "updating app widget")
+        //Toast.makeText(context, "Updating miWeather", Toast.LENGTH_LONG).show()
 
         // Construct the RemoteViews object
         val views = RemoteViews(context.packageName, R.layout.mi_weather_widget)
@@ -70,13 +78,66 @@ class MiWeatherWidget : AppWidgetProvider() {
         )
     }
 
+
+    private fun apiCall(context: Context) {
+        Log.d("MIWEATHER", "inside apiCall")
+
+        val apiUrl = "https://miel-api.arwebse.repl.co"
+        val city = "stockholm"
+        val verify = "5ecf486e3b8d878a4a87"
+        val freedom = false
+        val id = "android"
+
+        val url = "$apiUrl/weather?q=$city&verify=$verify&freedom=$freedom&id=$id";
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        httpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+//                    for ((name, value) in response.headers) {
+//                        println("$name: $value")
+//                    }
+
+                    Log.d("MIWEATHER", "got successfull response!")
+
+                    val jsonData = response.body!!.string()
+                    val result = Klaxon().toJsonString(jsonData)
+
+                    Log.d("MIWEATHER", "result: $result")
+
+                    //println(response.body!!.string())
+
+                    //el.cTemp.innerHTML = Math.round(res.current.temp);
+                    // get widget text from shared prefs
+                    //val prefs = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+                    //val tempString = prefs.getString("temperature", "x")
+
+                    val views = RemoteViews(context.packageName, R.layout.mi_weather_widget)
+                    views.setTextViewText(R.id.cTempView, "0")
+                    updateWidgets(context)
+
+                    Log.d("MIWEATHER", "set cTempView to 0")
+                }
+            }
+        })
+    }
+
     override fun onReceive(context: Context?, intent: Intent?) {
         super.onReceive(context, intent)
         // On receive intent broadcast
         val action = intent!!.action ?: ""
 
         if (context != null && action == "refresh") {
-            Log.d("MIWEATHER", "got increase broadcast")
+            Log.d("MIWEATHER", "got refresh request")
             // refresh weather data
 
             val views = RemoteViews(context.packageName, R.layout.mi_weather_widget)
@@ -88,6 +149,9 @@ class MiWeatherWidget : AppWidgetProvider() {
 
             val cTime = LocalDateTime.now()
             Log.d("MIWEATHER", "Time: $cTime")
+
+            apiCall(context)
+
 
             // update widgets
             updateWidgets(context)
